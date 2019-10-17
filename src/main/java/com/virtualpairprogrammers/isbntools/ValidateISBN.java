2400b652e9c9ea2493dc7f1e03985a29f9280de6
Enoch <walkerhaozheng@gmail.com>
2019-10-16 23:31:56 -0700

    Wring first tests

--- src/main/java/com/virtualpairprogrammers/isbntools/ValidateISBN.java
+++ src/main/java/com/virtualpairprogrammers/isbntools/ValidateISBN.java
@@ -2,8 +2,12 @@ package com.virtualpairprogrammers.isbntools;
 
 public class ValidateISBN {
 
-    public boolean checkISBN(int isbn) {
-        return true;
+    public boolean checkISBN(String isbn) {
+        int total = 0;
+        for (int i = 0; i < 10; i++) {
+            total += (isbn.charAt(i) * (10 - i));
+        }
+        return total % 11 == 0;
     }
 
 }
--- src/test/java/com/virtualprogrammers/isbntools/ExampleTests.java
+++ src/test/java/com/virtualprogrammers/isbntools/ExampleTests.java
@@ -1,7 +1,6 @@
 package com.virtualprogrammers.isbntools;
 
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
 import com.virtualpairprogrammers.isbntools.ValidateISBN;
@@ -11,14 +10,23 @@ public class ExampleTests {
     @Test
     public void checkAValidISBN() {
         ValidateISBN validator = new ValidateISBN();
-        boolean result = validator.checkISBN(140449116);
-        assertTrue(result);
+        boolean result = validator.checkISBN("0140449116");
+        assertTrue("first value", result);
+        result = validator.checkISBN("0140177396");
+        assertTrue("second value", result);
     }
 
     @Test
     public void checkAnInvalidISBN() {
         ValidateISBN validator = new ValidateISBN();
-        boolean result = validator.checkISBN(140449116);
+        boolean result = validator.checkISBN("0140449117");
+        assertFalse(result);
+    }
+
+    @Test
+    public void nineDigitISBNsAreNotAllowed() {
+        ValidateISBN validator = new ValidateISBN();
+        boolean result = validator.checkISBN("123456789");
         assertFalse(result);
     }
 
