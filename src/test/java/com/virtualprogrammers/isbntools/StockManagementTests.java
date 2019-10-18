f9239735adb053fab173f28fdf31217b9ff8f2d4
Enoch <walkerhaozheng@gmail.com>
2019-10-17 20:49:54 -0700

    Stubs

--- src/test/java/com/virtualprogrammers/isbntools/StockManagementTests.java
+++ src/test/java/com/virtualprogrammers/isbntools/StockManagementTests.java
@@ -3,14 +3,24 @@ package com.virtualprogrammers.isbntools;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-import com.virtualpairprogrammers.isbntools.StockManager;
+import com.virtualpairprogrammers.isbntools.*;
 
 public class StockManagementTests {
 
     @Test
     public void testCanGetACorrectLocatorCode() {
-        String isbn = "0140177396";
+
+        ExternalISBNDataService testService = new ExternalISBNDataService() {
+            @Override
+            public Book lookup(String isbn) {
+                return new Book(isbn, "Of Mice And Men", "J. Steinbeck");
+            }
+        };
+
         StockManager stockManager = new StockManager();
+        stockManager.setService(testService);
+
+        String isbn = "0140177396";
         String locatorCode = stockManager.getLocatorCode(isbn);
         assertEquals("7396J4", locatorCode);
     }
