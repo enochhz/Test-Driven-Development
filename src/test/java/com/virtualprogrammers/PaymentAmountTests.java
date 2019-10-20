73c04d4621ad558a0ac51f54134c413319ea6a8f
Enoch <walkerhaozheng@gmail.com>
2019-10-20 12:33:45 -0700

    Set up the new project

---
+++ src/test/java/com/virtualprogrammers/PaymentAmountTests.java
@@ -0,0 +1,15 @@
+package com.virtualprogrammers;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+
+import com.virtualpairprogrammers.*;
+
+public class PaymentAmountTests {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+}
